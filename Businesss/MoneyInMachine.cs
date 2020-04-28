18b7d660abe003083c64e3e314865741f10f8f27
stefanandy <31569796+stefanandy@users.noreply.github.com>
2020-04-28 20:32:13 +0300

    PaymentTerminal added. Tests for peyment terminal added.

---
+++ Businesss/MoneyInMachine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business
+{
+    public static class MoneyInMachine
+    {
+        public static double Coins { get; set; }
+    }
+}
